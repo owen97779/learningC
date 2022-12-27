de364cc4a590ceb7006e59613507e9f5ce160332
owen97779 <speedway45@live.co.uk>
2022-12-27 01:48:04 +0000

    Completed Section_15

---
+++ C Programming A Modern Approach/Section_15/Projects/5/calc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "stack.h"
+
+int main(void)
+{
+    
+    RPN();
+
+    return 0;
+}
---
+++ C Programming A Modern Approach/Section_15/Projects/5/stack.h
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+
+void stack_overflow(void);
+void stack_underflow(void);
+void RPN(void);
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
+void push(char i);
+float pop(void);
+void stack_overflow(void);
+void stack_underflow(void);
